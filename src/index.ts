89deab1b4dfaff4cf6dd66456d96403f7b1d4ccd
Paul Frazee <pfrazee@gmail.com>
2021-08-23 10:58:33 -0500

    Bind the adb client to specific databases

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export type { ApiBrokerServerHandlers } from './server.js'
 export { ApiBrokerServer } from './server.js'
 export { ApiBrokerClient } from './client.js'
-export { AtekDbRecordClient, AtekDbApiClient } from './record-client.js'
+export { AtekDbRecordClient, AtekDbApiClient } from './adb-client.js'
 export * from './registry.js'
 export * from './types.js'
